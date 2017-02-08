6831d76cecf84a0ca15465af68334aa0880c0ec9
Fluvio Lobo <fluvio.lobo@gmail.com>
2017-02-08 11:11:02 -0500

    Successfully implemented a logic switch for the scan tower to move in one direction or the opposite

--- Software/Arduino/readSwitch/readSwitch.ino
+++ Software/Arduino/readSwitch/readSwitch.ino
@@ -5,22 +5,34 @@
  * 02/07/2017
  */
 
+#define dPin1 2
+#define dPin2 3
+
 void setup()
 {
   Serial.begin( 115200 );
   Serial1.begin( 115200 );
+
+  pinMode(dPin1, INPUT);
+  pinMode(dPin2, INPUT);
   
 }
 
 void loop()
 {
 
-  Serial.println("G0 X100 Y-100 Z100");
-  Serial1.println("G0 X100 Y-100 Z100");  
-  delay(500);
+  if ( digitalRead(dPin1) == 1 )
+  {
+    Serial.println("G0 X100 Y-100 Z100");
+    Serial1.println("G0 X100 Y-100 Z100");
+  }
 
-  Serial.println("G0 X-100 Y100 Z-100");
-  Serial1.println("G0 X-100 Y100 Z-100");  
+  if ( digitalRead(dPin2) == 1 )
+  {
+    Serial.println("G0 X-100 Y100 Z-100");
+    Serial1.println("G0 X-100 Y100 Z-100");
+  }
+    
   delay(500);
 
 }
