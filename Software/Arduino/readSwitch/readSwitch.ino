bdfaaa5767eb2fcae0b3aeb82754a604069b2f6a
moe93 <m.odeh93@gmail.com>
2017-02-09 11:05:28 -0500

    failsafe. No endstops

--- Software/Arduino/readSwitch/readSwitch.ino
+++ Software/Arduino/readSwitch/readSwitch.ino
@@ -13,69 +13,84 @@
 #define DOWN 3
 
 #define grblBoard Serial1
+#define CAN (char)0x18    // ^x - Cancel, abort
+
+#define RESET_PIN 9
 
 char grblOutput[50];
+
 int strLen;
 
-void setup()
-{
+void setup() {
   Serial.begin( 115200 );
   grblBoard.begin( 115200 );
 
-  pinMode(UP, INPUT);
-  pinMode(DOWN, INPUT);
+  pinMode( UP,   INPUT );
+  pinMode( DOWN, INPUT );
 
-  grblBoard.println("$21=1"); // Activates hard limits (physical sensors)
-  while (grblBoard.available()) {
-    Serial.write(grblBoard.read());
+  grblBoard.println( "$RST=$" );
+  grblBoard.println( "$21=1" );                       // Activates hard limits (physical sensors)
+  //grblBoard.println("$22=1");
+  while ( grblBoard.available() ) {
+    Serial.write( grblBoard.read() );
   }
 
 }
 
 void loop() {
+  if ( digitalRead( UP ) ) {
+    Serial.println( "Sending:  G91 G0 X25 Y-25 Z25" );
+    Serial.println( "grblBoard Output: " );
+    grblBoard.println( "G91 G0 X25 Y-25 Z25" );
 
-  if (digitalRead(UP)) {
-    Serial.println("G91 G0 X23 Y-23 Z23");
-    Serial.println("M119");
-    Serial.println("grblBoard Output: ");
-    grblBoard.println("G91 G0 X24 Y-24 Z24");
-    while (grblBoard.available()) {
-      for (int i = 0; i < 50; i++) {
+    while ( grblBoard.available() ) {
+      for ( int i = 0; i < 50; i++ ) {
         grblOutput[i] = grblBoard.read();
-        if (!grblBoard.available()){
+
+        if ( !grblBoard.available() ) {
           strLen = i;
           break;
         }
       }
     }
-    for (int i=0; i<strLen; i++){
-      Serial.print(grblOutput[i]);
+
+    for ( int i = 0; i < strLen; i++) {
+      Serial.print( grblOutput[i] );
+    } Serial.print( "\n" );
+    for (int i = 0; i < strLen; i++) {
+      if (grblOutput [i] == 'A') {
+        Serial.println("Resetting");
+        grblBoard.println( "$RST=$" );
+      }
     }
-    Serial.print("\n");
   }
 
 
-  else if (digitalRead(DOWN))
-  {
-    Serial.println("G91 G0 X-25 Y25 Z-25");
-    Serial.println("M119");
-    Serial.println("grblBoard Output: ");
-    grblBoard.println("G91 G0 X-24 Y24 Z-24");
-    while (grblBoard.available()) {
-      for (int i = 0; i < 50; i++) {
+  else if ( digitalRead( DOWN ) ) {
+    Serial.println( "Sending:  G91 G0 X-25 Y25 Z-25" );
+    Serial.println( "grblBoard Output: " );
+    grblBoard.println( "G91 G0 X-25 Y25 Z-25" );
+
+    while ( grblBoard.available() ) {
+      for ( int i = 0; i < 50; i++ ) {
         grblOutput[i] = grblBoard.read();
-        if (!grblBoard.available()){
+
+        if ( !grblBoard.available() ) {
           strLen = i;
           break;
         }
       }
     }
-    for (int i=0; i<strLen; i++){
-      Serial.print(grblOutput[i]);
-    }
-    Serial.print("\n");
-  }
 
-  delay( 250 );
+    for ( int i = 0; i < strLen; i++) {
+      Serial.print( grblOutput[i] );
+    } Serial.print( "\n" );
+    for (int i = 0; i < strLen; i++) {
+      if (grblOutput [i] == 'A') {
+        Serial.println("Resetting");
+        grblBoard.println( "$RST=$" );
+      }
+    }
+  } delay( 250 );
 
 }
