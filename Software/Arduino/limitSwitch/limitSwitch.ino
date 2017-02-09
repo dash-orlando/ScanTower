085c87472339ba7f3af2ce54e4e99dac9a3ceea9
moe93 <m.odeh93@gmail.com>
2017-02-09 13:08:12 -0500

    Upper & lower endstops work

--- Software/Arduino/limitSwitch/limitSwitch.ino
+++ Software/Arduino/limitSwitch/limitSwitch.ino
@@ -5,22 +5,23 @@
  * 02/07/2017
  */
 
-#define dPin1       2
-#define dPin2       3
-#define limitPin1   8
-#define resetPin    9
-#define estopPin    10
+#define dPin1             2
+#define dPin2             3
+#define limitPinBOTTOM    8
+#define resetPin          9
+#define estopPin          10
+#define limitPinTOP       11
 
 void setup()
 {
   Serial.begin( 115200 );
   Serial1.begin( 115200 );
 
-  pinMode(dPin1,     INPUT);
-  pinMode(dPin2,     INPUT);
-  pinMode(limitPin1, INPUT);
-  pinMode(resetPin, OUTPUT);
-  pinMode(estopPin, OUTPUT);
+  pinMode(dPin1,          INPUT);
+  pinMode(dPin2,          INPUT);
+  pinMode(limitPinBOTTOM, INPUT);
+  pinMode(limitPinTOP,    INPUT);
+  pinMode(estopPin,       OUTPUT);
   
 }
 
@@ -28,34 +29,39 @@ void loop()
 {
 
   // Down
-  if ( digitalRead(dPin1) == 1 && digitalRead(limitPin1) == 0 )
+  if ( digitalRead(dPin1) == 1 && digitalRead(limitPinBOTTOM) == 0 )
   {
-    Serial.println("G91 G0 X5 Y-5 Z5");
-    Serial1.println("G91 G0 X5 Y-5 Z5");
+    Serial.println("G91 G0 X15 Y-15 Z15");
+    Serial1.println("G91 G0 X15 Y-15 Z15");
   }
 
   // Up
-  if ( digitalRead(dPin2) == 1 )
+  else if ( digitalRead(dPin2) == 1 && digitalRead(limitPinTOP) == 0 )
   {
-    Serial.println("G91 G0 X-5 Y5 Z-5");
-    Serial1.println("G91 G0 X-5 Y5 Z-5");
+    Serial.println("G91 G0 X-15 Y15 Z-15");
+    Serial1.println("G91 G0 X-15 Y15 Z-15");
   }
 
   // Down Limit
-  if ( digitalRead(limitPin1) == 1 )
+  if ( digitalRead(limitPinBOTTOM) == 1 )
   {
     digitalWrite(estopPin, HIGH);
     //delay(50);
     digitalWrite(estopPin, LOW);
-    //delay(50);
-    digitalWrite(resetPin, HIGH);
-    //delay(50);
-    digitalWrite(resetPin, HIGH);
-    //delay(50);
     Serial.println("G91 G0 X-25 Y25 Z-25");
     Serial1.println("G91 G0 X-25 Y25 Z-25");
   }
+
+  // Up Limit
+  else if ( digitalRead(limitPinTOP) == 1 )
+  {
+    digitalWrite(estopPin, HIGH);
+    //delay(50);
+    digitalWrite(estopPin, LOW);
+    Serial.println("G91 G0 X25 Y-25 Z25");
+    Serial1.println("G91 G0 X25 Y-25 Z25");
+  }
     
-  delay(100);
+  delay(150);
 
 }
